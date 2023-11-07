1cef73d45dac6f7de70d8d7b88af1d8bec5f528c
henriqueAlgauer <algauer.henrique@gmail.com>
2023-11-06 23:18:42 -0300

    insertionSort

--- selectionSort.js
+++ selectionSort.js
@@ -5,12 +5,8 @@ for (let atual=0; atual < livros.length; atual++){
     let menor = menorValor(livros, atual)
 
     let livroAtual = livros[atual]
-    console.log('livro atual', livros[atual]);
     let livroMenorPreco = livros[menor]
-    console.log('livro menor preço', livros[menor]);
 
     livros[atual] = livroMenorPreco
     livros[menor] = livroAtual
 }
-
-console.log(livros);
